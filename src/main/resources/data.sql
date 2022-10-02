0cda58ced929e3352661be02a3d7efaf8b3c8b95
Abdulkerim SEKILI <kerimsekilix@gmail.com>
2022-10-02 16:36:41 +0300

    Adding database with H2(schema.sql)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into t_owner(id,first_name,last_name) values (1,"Ziya","Emmi");
+
+insert into t_pet(id,pet_name,birth_date,owner_id) values (1,"Cicikus","2022,09,07",1)
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,18 @@
+create table public.t_owner(
+    id int not null ,
+    first_name varchar(50),
+    last_name varchar(50)
+);
+create table public.t_pet(
+    id int not null ,
+    pet_name varchar(50),
+    birth_date date,
+    owner_id int
+
+);
+
+alter table public.t_owner add constraint public.constraint_1 primary key(id);
+alter table public.t_pet add constraint public.constraint_2 primary key (id);
+alter table public.t_pet add constraint public.constraint_3 foreign key (owner_id) references public.t_owner(id);
+create sequence public.petclinic_sequence start with (100);
+;
