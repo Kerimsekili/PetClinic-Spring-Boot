3cb86434235b1005ad2ec283dc45af3ff71a0860
Abdulkerim SEKILI <kerimsekilix@gmail.com>
2022-10-06 13:48:21 +0300

    Adding Transaction

--- src/main/java/com/example/petclinic/service/PetClinicServiceImpl.java
+++ src/main/java/com/example/petclinic/service/PetClinicServiceImpl.java
@@ -1,21 +1,31 @@
 package com.example.petclinic.service;
 
 import com.example.petclinic.dao.OwnerRepository;
+import com.example.petclinic.dao.PetRepository;
 import com.example.petclinic.exception.OwnerNotFoundException;
 import com.example.petclinic.model.Owner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class PetClinicServiceImpl implements PetClinicService{
 
     private OwnerRepository ownerRepository;
 
+    private PetRepository petRepository;
+
     @Autowired
     public void setOwnerRepository(OwnerRepository ownerRepository) {
         this.ownerRepository = ownerRepository;
+    }
+
+    @Autowired
+    public void setPetRepository(PetRepository petRepository) {
+        this.petRepository = petRepository;
     }
 
     @Override
@@ -47,6 +57,8 @@ public class PetClinicServiceImpl implements PetClinicService{
 
     @Override
     public void deleteOwner(Long id) {
+        petRepository.deleteByOwnerId(id);
         ownerRepository.delete(id);
+        //if (true) throw new RuntimeException("testing rollback");
     }
 }
